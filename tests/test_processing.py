c38252c66846f7e8f7c4c396f2c6b29903a5b222
Katya <gorbunovakatja530@gmail.com>
2025-02-05 16:09:11 +0300

    NEV

---
+++ tests/test_processing.py
@@ -0,0 +1,33 @@
+# tests/test_processing.py
+
+from src.processing import filter_by_state, sort_by_date
+
+data = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+]
+
+
+def test_filter_by_state():
+    assert filter_by_state(data) == [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    ]
+    assert filter_by_state(data, 'CANCELED') == [
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+    ]
+
+
+def test_sort_by_date():
+    sorted_data = sort_by_date(data)
+    assert sorted_data == [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    ]
+    sorted_data_asc = sort_by_date(data, reverse=False)
+    assert sorted_data_asc == sorted_data[::-1]
