5fca3baf2b4de97325d58f48b3c10590b1ff5771
Katya <gorbunovakatja530@gmail.com>
2025-03-23 16:44:07 +0300

    исправление пути

--- tests/test_transactions_reader.py
+++ tests/test_transactions_reader.py
@@ -1,15 +1,16 @@
 import pandas as pd
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from src.transactions_reader import read_transaction_csv, read_transaction_excel
 
 
-def test_read_transaction_csv_success():
+@patch("pandas.read_csv")
+def test_read_transaction_csv_success(mock_readr_csv):
     # Мокируем pd.read_csv чтобы вернуть DataFrame
-    mock_df = pd.DataFrame([{'date': '2023-11-15', 'amount': 100, 'description': 'Test'}])
-    with patch('transactions_reader.pd.read_csv', return_value=mock_df):
-        result = read_transaction_csv('dummy.csv')
-        assert result == [{'date': '2023-11-15', 'amount': 100, 'description': 'Test'}]
-
+    mock_readr_csv.return_value = pd.DataFrame({'date': ['2023-11-15'], 'amount': [100], 'description': ['Test']})
+    #with patch('transactions_reader.pd.read_csv', return_value=mock_df):
+    #    result = read_transaction_csv('dummy.csv')
+    result = read_transaction_csv("test_csv")
+    assert result == [{'date': '2023-11-15', 'amount': 100, 'description': 'Test'}]
 
 def test_read_transaction_csv_file_not_found():
     # Проверяем, что возвращается пустой список и выводится сообщение об ошибке
@@ -20,7 +21,7 @@ def test_read_transaction_csv_file_not_found():
 def test_read_transaction_excel_success():
     # Мокируем pd.read_excel
     mock_df = pd.DataFrame([{'date': '2023-11-16', 'amount': 200, 'description': 'Test Excel'}])
-    with patch('transactions_reader.pd.read_excel', return_value=mock_df):
+    with patch('src.transactions_reader.pd.read_excel', return_value=mock_df):
         result = read_transaction_excel('dummy.xlsx')
         assert result == [{'date': '2023-11-16', 'amount': 200, 'description': 'Test Excel'}]
 
